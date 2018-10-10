e837033e6da6f6250c3f2452da65692a5c3d92ba
Patryk Kotlarz <pkotlarz@suncode.pl>
2018-10-10 10:57:54 +0200

    Logs directory fix and backup names change

--- src/main/java/com/kotlarz/fly4freenotifier/service/backup/DatabaseBackupService.java
+++ src/main/java/com/kotlarz/fly4freenotifier/service/backup/DatabaseBackupService.java
@@ -9,12 +9,14 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 
 @Slf4j
 @Service
-public class DatabaseBackupService {
+public class DatabaseBackupService
+{
     private static final Integer MAX_BACKUP_DAYS = 7;
 
     private static final String BACKUPS_FOLDER_NAME = "backups";
@@ -22,36 +24,58 @@ public class DatabaseBackupService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    private SimpleDateFormat dateTimeFormatter = new SimpleDateFormat( "yyyy-MM-dd_HH_mm_ss" );
+
     @Scheduled( fixedRate = 1000 * 3600 * 12 )
-    public void runBackup() {
+    public void runBackup()
+    {
         createBackup();
         cleanupBackups();
     }
 
-    private void createBackup() {
-        log.info("Creating database backup");
+    private void createBackup()
+    {
+        log.info( "Creating database backup" );
         Date now = new Date();
-        String fileName = "backup_" + now.getTime() + ".zip";
-        jdbcTemplate.execute("BACKUP TO '" + BACKUPS_FOLDER_NAME + "/" + fileName + "'");
+        String fileName = "backup_" + dateTimeFormatter.format( now ) + ".zip";
+        jdbcTemplate.execute( "BACKUP TO '" + BACKUPS_FOLDER_NAME + "/" + fileName + "'" );
     }
 
-    private void cleanupBackups() {
-        log.info("Removing old backups");
+    private void cleanupBackups()
+    {
+        log.info( "Removing old backups" );
         DateTime now = DateTime.now();
-        DateTime maxMinusDate = now.minusDays(MAX_BACKUP_DAYS);
+        DateTime maxMinusDate = now.minusDays( MAX_BACKUP_DAYS );
 
-        Arrays.asList(new File(BACKUPS_FOLDER_NAME).listFiles()).forEach(file -> {
-            String fileName = FilenameUtils.removeExtension(file.getName());
-            DateTime backupDate = getBackupDate(fileName);
-
-            if (backupDate.isBefore(maxMinusDate)) {
-                log.info("Removing file '{}'", fileName);
+        Arrays.asList( new File( BACKUPS_FOLDER_NAME ).listFiles() ).forEach( file -> {
+            try
+            {
+                DateTime backupDate = getBackupDate( file.getName() );
+                if ( backupDate.isBefore( maxMinusDate ) )
+                {
+                    log.info( "Removing file '{}'", file.getName() );
+                    file.delete();
+                }
+            }
+            catch ( IllegalBackupFileNameException ex )
+            {
+                log.error( "File '" + file.getName() + "' has incorrect file name format. Deleting", ex );
                 file.delete();
             }
-        });
+        } );
     }
 
-    private DateTime getBackupDate(String fileName) {
-        return new DateTime(Long.parseLong(fileName.split("_")[1]));
+    private DateTime getBackupDate( String fileName )
+                    throws IllegalBackupFileNameException
+    {
+        try
+        {
+            String withoutExtension = FilenameUtils.removeExtension( fileName );
+            return new DateTime( dateTimeFormatter.parse( withoutExtension.replace( "backup_", "" ) ) );
+        }
+        catch ( Exception ex )
+        {
+            throw new IllegalBackupFileNameException( ex );
+        }
     }
 }
---
+++ src/main/java/com/kotlarz/fly4freenotifier/service/backup/IllegalBackupFileNameException.java
@@ -0,0 +1,10 @@
+package com.kotlarz.fly4freenotifier.service.backup;
+
+public class IllegalBackupFileNameException
+                extends Exception
+{
+    public IllegalBackupFileNameException( Throwable ex )
+    {
+        super( ex );
+    }
+}
